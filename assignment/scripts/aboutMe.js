b84a9f0485161aac8e7bf01aa1cd2bd73eeb0aa3
Todd McCoy <tcmccoy@gmail.com>
2022-05-11 14:07:49 -0500

    started first few lines of javascript code

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,13 +1,13 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-
+let firstName = 'Todd';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'McCoy';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-
+let fullName = firstName + lastName
 // 4 - Console log the value of `fullName`
-
+console.log ( fullName )
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 
 // 6 - Console log this sentence, adding in the variables you created above: 
